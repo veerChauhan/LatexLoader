f521eab041db1c74226494f02f7aa00ef7d5da33
Ravi Ranjan <r.ranjanchn@gmail.com>
2024-02-17 15:35:04 +0530

    Configure Root, Removed storyBoard

--- Example/LatexKatexView/AppDelegate.swift
+++ Example/LatexKatexView/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.configureRoot()
         return true
     }
 
@@ -42,5 +43,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+    
+    private func configureRoot() {
+        let root = ExampleListViewController()
+        let naviagtion = UINavigationController(rootViewController: root)
+        naviagtion.setNavigationBarHidden(true, animated: false)
+        window = UIWindow()
+        window?.rootViewController = naviagtion
+        self.window?.makeKeyAndVisible()
+    }
 }
 
---
+++ Example/LatexKatexView/ExampleListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExampleListViewController.swift
+//  LatexKatexView_Example
+//
+//  Created by Ravi Ranjan on 17/02/24.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ExampleListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
